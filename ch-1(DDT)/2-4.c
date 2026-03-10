2645b31f5a08cb8446eadbe18811bf10f1635bec
김정민 <wjdala02@gmail.com>
2026-03-10 12:00:59 +0900

    2026/03/10

---
+++ ch-1(DDT)/2-4.c
@@ -0,0 +1,25 @@
+
+#include<stdio.h>
+
+int main()
+{
+	int a = 1234;
+	printf("[%d]\n", a);
+	printf("[%8d]\n", a);
+	printf("[%8-d]\n", a);
+	printf("[%08d]\n", a);
+	printf("[%+08d]\n", a);
+
+	char b = 'A';
+	double c = 3.14159;
+
+	printf("[%c]\n", b);
+	printf("[%8c]\n", b);
+	printf("[%-8c]\n", b);
+	printf("[%f]\n", b);
+	printf("[%c8.3f]\n", b);
+	printf("[%c+8.3f]\n", b);
+	printf("[%08.3f]\n", b);
+	printf("[%+08.3fc]\n", b);
+
+}
